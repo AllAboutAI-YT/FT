4c49075a936fdff57968a25eca1d7a00b61e1414
Kris <120593050+AllAboutAI-YT@users.noreply.github.com>
2023-02-02 11:58:54 +0100

    Add files via upload

---
+++ prepare_finetune_data.py
@@ -0,0 +1,25 @@
+import os
+import json
+
+
+src_dir = 'completion/'
+prompt_dir = 'prompts/'
+
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+
+
+if __name__ == '__main__':
+    files = os.listdir(src_dir)
+    data = list()
+    for file in files:
+        completion = open_file(src_dir + file)
+        prompt = open_file(prompt_dir + file)
+        info = {'prompt': prompt, 'completion': completion}
+        data.append(info)
+    with open('plots.jsonl', 'w') as outfile:
+        for i in data:
+            json.dump(i, outfile)
+            outfile.write('\n')
